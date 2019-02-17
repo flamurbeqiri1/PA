7a95b445b871fc3aad9f807d31b28d6063fcb845
Flamur Beqiri <flamurbeqiri1@gmail.com>
2019-02-17 12:57:13 +0100

    Album VC, refactored to separate storyboard albums, loading dummy data from storyboard

---
+++ PA/PA/ViewControllers/Albums/AlbumsTableViewController.swift
@@ -0,0 +1,47 @@
+//
+//  AlbumsTableViewController.swift
+//  PA
+//
+//  Created by Flamur Beqiri on 17/02/2019.
+//  Copyright © 2019 Flamur Beqiri. All rights reserved.
+//
+
+import UIKit
+
+class AlbumsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+// MARK: - Table view data source
+
+extension AlbumsTableViewController {
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AlbumTableViewCell", for: indexPath)
+        // Configure the cell...
+        return cell
+    }
+}
