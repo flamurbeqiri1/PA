dc075a6d2d736ac30fc5218ff7a89de4b47e94af
Flamur Beqiri <flamurbeqiri1@gmail.com>
2019-02-17 12:40:08 +0100

    Refactored Posts storyboard into separate storyboard, added VC class and cell, auto-layout for cell displaying title and body

---
+++ PA/PA/ViewControllers/Posts/PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  PA
+//
+//  Created by Flamur Beqiri on 17/02/2019.
+//  Copyright © 2019 Flamur Beqiri. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
