fe898bfd3c05244fd060c0bf77c8009085e1b45b
Flamur Beqiri <flamurbeqiri1@gmail.com>
2019-02-17 18:43:14 +0100

    Added constant file

---
+++ PA/PA/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  PA
+//
+//  Created by Flamur Beqiri on 17/02/2019.
+//  Copyright © 2019 Flamur Beqiri. All rights reserved.
+//
+
+import Foundation
+
+public let baseUrl = "https://jsonplaceholder.typicode.com/"
