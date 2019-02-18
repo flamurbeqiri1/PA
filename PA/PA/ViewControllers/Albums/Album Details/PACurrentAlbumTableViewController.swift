bfd42a14e331b5fdeb7993b0bd7bdfa57305e121
Flamur Beqiri <flamurbeqiri1@gmail.com>
2019-02-18 13:04:28 +0100

    Plugged albumService into VC to load data from album and image

--- PA/PA/ViewControllers/Albums/Album Details/PACurrentAlbumTableViewController.swift
+++ PA/PA/ViewControllers/Albums/Album Details/PACurrentAlbumTableViewController.swift
@@ -8,14 +8,18 @@
 
 import UIKit
 
-class PACurrentAlbumTableViewController: UITableViewController {
+class PACurrentAlbumTableViewController: UITableViewController, HasDependencies {
 
+    // Services
+    private lazy var albumService: AlbumService = dependencies.albumService()
+    // Properties
+    var currentAlbum: [Album]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -23,7 +27,6 @@ class PACurrentAlbumTableViewController: UITableViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
 
 }
 
@@ -36,14 +39,28 @@ extension PACurrentAlbumTableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return self.currentAlbum.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "PACurrentAlbumTableViewCell", for: indexPath) as? PACurrentAlbumTableViewCell else {
             return UITableViewCell()
         }
-        // Configure the cell...
+        let albumPhotoData = self.currentAlbum[indexPath.row]
+        cell.albumTitleLabel.text = albumPhotoData.title
+        if let albumThumbnailUrl = albumPhotoData.thumbnailUrl {
+            self.albumService.loadImage(from: albumThumbnailUrl, into: cell.albumThumbnailImage) { (result) in
+                switch result {
+                case .success:
+                    break // nothing to do, image loaded into ImageView
+                case .failure(let error):
+                    print("DEBUG: Load image failed with error: \(error)")
+                }
+            }
+        } else {
+             print("DEBUG: Thumbnail Image url not found from album")
+        }
+        
         return cell
     }
 }
